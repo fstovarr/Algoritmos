51cb5e9632361b9e1e5bfbe7b6347520ccc3f67a
fstovarr <fstovarr@unal.edu.co>
2018-09-12 10:46:17 -0500

    Update fibonacci.c

--- Laboratorios/Laboratorio2/fibonacci.c
+++ Laboratorios/Laboratorio2/fibonacci.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 
+typedef enum { false, true } bool;
+
 void sshort() {
     printf("SHORT: ");
     int i, p = 0, a = 0;
-    for (i = 0; i <= 50; i++){
+    for (i = 0; i <= 100; i++){
         short prev = 0, act = 1, res = 0;
         int j;
         for (j = 0; j < i; j++) {
@@ -24,7 +26,7 @@ void sshort() {
 void iinteger() {
     printf("INTEGER: ");
     int i, p = 0, a = 0;
-    for (i = 0; i <= 50; i++){
+    for (i = 0; i <= 100; i++){
         int prev = 0, act = 1, res = 0;
         int j;
         for (j = 0; j < i; j++) {
@@ -44,7 +46,7 @@ void iinteger() {
 void llong() {
 printf("LONG: ");
     long i, p = 0, a = 0;
-    for (i = 0; i <= 50; i++){
+    for (i = 0; i <= 100; i++){
         long prev = 0, act = 1, res = 0;
         int j;
         for (j = 0; j < i; j++) {
@@ -64,8 +66,9 @@ printf("LONG: ");
 
 void longlong() {
     printf("LONG LONG: ");
-    long long i, p = 0, a = 0;
-    for (i = 0; i <= 50; i++){
+    int i = 0;
+    long long p = 0, a = 0;
+    for (i = 0; i <= 100; i++){
         long long prev = 0, act = 1, res = 0;
         int j;
         for (j = 0; j < i; j++) {
@@ -84,11 +87,11 @@ void longlong() {
 }
 
 void ffloat() {
-
-printf("FLOAT: ");
+    printf("FLOAT: ");
     int i;
     float p = 0, a = 0;
-    for (i = 0; i <= 50; i++){
+
+    for (i = 0; i <= 186; i++){
         float prev = 0, act = 1, res = 0;
         int j;
         for (j = 0; j < i; j++) {
@@ -99,18 +102,20 @@ printf("FLOAT: ");
         a = res;
 
         if(a < p) {
-            printf("Overflow to FLOAT in fib(%d)\nLast value: fib(%d) = %d\n\n", i, i-1, p);
+            printf("Overflow to FLOAT in fib(%f)\nLast value: fib(%f) = %f\n\n", i, i-1, p);
             break;
         }
         p = a;
     }
+
+    printf("Float variable doesn't overflow, but with big numbers the precision is lost. For larger values than fib(%d), the result is %f\n\n", (i - 1), a);
 }
 
 void ddouble() {
 printf("DOUBLE: ");
     int i;
     double p = 0, a = 0;
-    for (i = 0; i <= 50; i++){
+    for (i = 0; i <= 1476; i++){
         double prev = 0, act = 1, res = 0;
         int j;
         for (j = 0; j < i; j++) {
@@ -120,18 +125,21 @@ printf("DOUBLE: ");
         }
         a = res;
         if(a < p) {
-            printf("Overflow to DOUBLE in fib(%d)\nLast value: fib(%d) = %d\n\n", i, i-1, p);
+            printf("Overflow to DOUBLE in fib(%d)\nLast value: fib(%d) = %f\n\n", i, i-1, p);
             break;
         }
+        //printf("fib(%d) = %f\n",i,a);
         p = a;
     }
+
+    printf("Double variable doesn't overflow, but with big numbers the precision is lost. For larger values than fib(%d), the result is %f\n\n", (i - 1), a);
 }
 
 void ldouble() {
-    printf("LONG DOUBLE: ");
+printf("LONG DOUBLE: ");
     int i;
     long double p = 0, a = 0;
-    for (i = 0; i <= 50; i++){
+    for (i = 0; i <= 100; i++){
         long double prev = 0, act = 1, res = 0;
         int j;
         for (j = 0; j < i; j++) {
@@ -140,9 +148,11 @@ void ldouble() {
             act = res;
         }
         a = res;
-            printf("Overflow to LONG DOUBLE in fib(%d)\nLast value: fib(%d) = %d\n\n", i, i-1, p);
+        if(a < p) {
+            printf("Overflow to LONG DOUBLE in fib(%d)\nLast value: fib(%d) = %f\n\n", i, i-1, p);
             break;
         }
+        printf("fib(%d) = %Lf\n",i,a);
         p = a;
     }
 }
